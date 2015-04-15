e04deb482964afa030e6617d7bf495525703cba9
Michael Swartz <mjswartz@gmail.com>
2015-04-15 11:29:19 -0400

    fixes hound syntax issues

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -3,10 +3,10 @@ class CompetitionsController < ApplicationController
     [:new, :create, :edit, :update, :destroy]
 
     STATES = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
-      'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI',
-      'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY',
-      'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT',
-      'WA', 'WI', 'WV', 'WY']
+              'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+              'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
+              'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
+              'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
 
   def index
     @competitions = Competition.order(start_date: :desc)
@@ -18,12 +18,12 @@ class CompetitionsController < ApplicationController
 
   def new
     @competition = Competition.new
-    @STATES = STATES
+    @states = STATES
   end
 
   def create
     @competition = current_user.competitions.new(competition_params)
-    @STATES = STATES
+    @states = STATES
 
     if @competition.save
       flash[:notice] = 'New competition created'
@@ -36,12 +36,12 @@ class CompetitionsController < ApplicationController
 
   def edit
     @competition = Competition.find(params[:id])
-    @STATES = STATES
+    @states = STATES
   end
 
   def update
     @competition = Competition.find(params[:id])
-    @STATES = STATES
+    @states = STATES
 
     if @competition.update(competition_params)
       flash[:notice] = 'Competition updated'
