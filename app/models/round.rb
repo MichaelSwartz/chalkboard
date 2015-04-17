e5aadc9a237fad63915340ca2403e3775d2816d4
Michael Swartz <mjswartz@gmail.com>
2015-04-17 11:00:17 -0400

    scoring methods

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,10 +1,16 @@
 class Round < ActiveRecord::Base
   belongs_to :competition
   has_many :routes
+  has_many :attempts, through: :routes
+  has_many :athletes, through: :attempts
 
   validates :competition, presence: true
   validates :name, presence: true
   validates :number,
     presence: true,
     numericality: { only_integer: true }
+
+  def leaderboard
+    athletes
+  end
 end
